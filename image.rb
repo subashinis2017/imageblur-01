55d3b0ff768af6c983a51efa84b6a87ee21f5294
Subashini <subashinis2017@gmail.com>
2019-07-23 23:13:06 +0000

    Image blur1 challenge

---
+++ image.rb
@@ -0,0 +1,21 @@
+class Image
+    def initialize(array)
+        @array = array
+    end
+    def output_image
+        @array.each do |row|
+            puts row.join
+        end
+    end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+
+
+image.output_image
